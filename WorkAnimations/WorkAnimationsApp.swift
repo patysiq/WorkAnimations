0ef1993305fe542d82b34d96c2c63c25842af944
Patricia <patysiq@yahoo.com.br>
2021-02-13 11:31:53 -0300

    Initial Commit

---
+++ WorkAnimations/WorkAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkAnimationsApp.swift
+//  WorkAnimations
+//
+//  Created by PATRICIA S SIQUEIRA on 13/02/21.
+//
+
+import SwiftUI
+
+@main
+struct WorkAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
